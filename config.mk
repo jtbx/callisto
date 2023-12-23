e2f390271db1921f4fbbfa26008aa9322f8606d6
Jeremy Baxter <jtbx@duck.com>
2023-12-24 05:53:29 +1300

    move PREFIX and MANPREFIX into config.mk

--- config.mk
+++ config.mk
@@ -1,7 +1,10 @@
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/man
+
 CC       = cc
 CFLAGS   = -std=c99 -pedantic -fpic -O2 -Wall -Wextra -Wno-override-init -Iexternal/luasrc
 CPPFLAGS = -D_DEFAULT_SOURCE
-LDFLAGS = -lm
+LDFLAGS  = -lm
 
 # Enable readline
 #CPPFLAGS += -DLUA_USE_READLINE
