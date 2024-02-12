ba8effbead7d21c395b462828083fcc337bc3403
Jeremy Baxter <jeremy@baxters.nz>
2024-02-12 15:34:26 +1300

    flake: fix style

--- flake.nix
+++ flake.nix
@@ -1,40 +1,41 @@
 {
-	description = "Callisto, a featureful extension runtime for Lua 5.4";
+  description = "Callisto, a featureful extension runtime for Lua 5.4";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-	outputs = { self, nixpkgs }:
-	let
-		systems = [
-			"x86_64-linux"
-			"aarch64-linux"
-		];
-		forAllSystems = fn:
-			nixpkgs.lib.genAttrs systems (system:
-				fn (import nixpkgs {
-					inherit system;
-				})
-			);
-	in
-	{
-		packages = forAllSystems (pkgs: {
-			default = pkgs.stdenv.mkDerivation {
-				name = "callisto";
-				src = ./.;
-				nativeBuildInputs = with pkgs; [
-					gcc
-					binutils
-				];
-				buildPhase = ''
-					make
-				'';
-				installPhase = ''
-					mkdir -p $out/bin
-					make DESTDIR="$out" PREFIX=/ install
-				'';
-			};
-		});
-	};
+  outputs = { self, nixpkgs }:
+  let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    forAllSystems = fn:
+      nixpkgs.lib.genAttrs systems (system:
+        fn (import nixpkgs {
+          inherit system;
+        })
+      );
+  in
+  {
+    packages = forAllSystems (pkgs: {
+      default = pkgs.stdenv.mkDerivation {
+        name = "callisto";
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          gcc
+          binutils
+        ];
+        buildPhase = ''
+          make
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          make DESTDIR="$out" PREFIX=/ install
+        '';
+      };
+    });
+  };
 }
